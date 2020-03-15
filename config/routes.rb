83cd27b7c673273e1694a81e82673ded0670c489
Kit L <kitliew21@gmail.com>
2020-03-15 21:37:49 +0000

    scaffold product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   get 'about_us/contact'
   get 'home/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_201106) do
+ActiveRecord::Schema.define(version: 2020_03_15_212755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,13 +21,21 @@ ActiveRecord::Schema.define(version: 2020_03_03_201106) do
     t.string "start_time"
     t.string "end_time"
     t.string "description"
+    t.string "place_name"
     t.float "lat"
     t.float "long"
     t.string "street"
     t.string "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "place_name"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
