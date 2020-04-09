2c5a7753ec601c9cfd7525c26d581e8e38c38d74
Sarah Rudston <sarah.rudston@gmail.com>
2020-04-09 19:23:59 +0100

    infobox on Google Map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   def load_events
     @events = Event.all
     @events_default = Gmaps4rails.build_markers(@events) do |plot, marker|
+      events_path = view_context.link_to plot.title, events_path(plot)
       marker.lat plot.lat
       marker.lng plot.long
+      marker.infowindow "<b>#{events_path}</b>"
     end
   end
 
