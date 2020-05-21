258e56b8c285913dce4c187e13a3c5a842467ddc
Kit L <kitliew21@gmail.com>
2020-05-21 19:01:50 +0100

    error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_133345) do
+ActiveRecord::Schema.define(version: 2020_05_02_141210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,14 +31,6 @@ ActiveRecord::Schema.define(version: 2020_05_17_133345) do
     t.string "city"
     t.string "category"
     t.bigint "fbid"
-  end
-
-  create_table "feeds", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
